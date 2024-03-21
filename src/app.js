cfc8c2b6ab1dbc55836568ecaae5b91ac782a2c0
Camilo Figueroa <cigotete@gmail.com>
2024-03-21 18:49:47 -0500

    Implementing layout engine.

--- src/app.js
+++ src/app.js
@@ -1,23 +1,32 @@
+import layout from "../public/layout.html";
+import {
+  constructRoutes,
+  constructApplications,
+  constructLayoutEngine,
+} from "single-spa-layout";
 import { registerApplication, start } from "single-spa";
 
-registerApplication({
-  name: "app-mf-javascript",
-  app: window.appJavascript,
-  activeWhen: ["/"],
-  customProps: {
-    codigo: "001",
-    nombre: "Microfrontend en Javascipt"
+const routes = constructRoutes(layout);
+
+const applications = constructApplications({
+  routes,
+  async loadApp({ name }) {
+
+    if (window[name]) {
+      return window[name];
+    } else {
+      throw Error(
+        `Could not find global variable 'window["${name}"]'. Try running pnpm start -- 06-${name}`
+      );
+    }
   }
+})
+
+const layoutEngine = constructLayoutEngine({
+  routes,
+  applications,
 });
 
-registerApplication({
-  name: "app-mf-react",
-  app: window.appReact,
-  activeWhen: ["/"],
-  customProps: {
-    codigo: "002",
-    nombre: "Microfrontend en React"
-  }
-});
+applications.forEach(registerApplication);
 
 start();
